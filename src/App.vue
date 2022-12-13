416adfecaf60b351ef9366ca5d4c2f9fe58863a1
unryeong <leyng7@gmail.com>
2022-12-14 00:10:42 +0900

    feat: add vuetify.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
 </template>
 
 <script>
@@ -15,12 +14,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    touch-action: pan-x pan-y;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-text-field label="대출금액" v-model="money"></v-text-field>
+    <div class="d-flex" style="gap: 0.5rem">
+      <v-btn variant="tonal" @click="incrementMoney(100000)">십만원</v-btn>
+      <v-btn variant="tonal" @click="incrementMoney(1000000)">백만원</v-btn>
+      <v-btn variant="tonal" @click="incrementMoney(10000000)">천만원</v-btn>
+      <v-btn color="error" variant="tonal" @click="this.money = 0">정정</v-btn>
+    </div>
+  </div>
+  <div>
+    <v-text-field label="대출기간" v-model="period"></v-text-field>
+    <div class="d-flex" style="gap: 0.5rem">
+      <v-btn variant="tonal" @click="incrementPeriod(1)">1개월</v-btn>
+      <v-btn variant="tonal" @click="incrementPeriod(6)">6개월</v-btn>
+      <v-btn variant="tonal" @click="incrementPeriod(12)">1년</v-btn>
+      <v-btn variant="tonal" @click="incrementPeriod(24)">2년</v-btn>
+      <v-btn color="error" variant="tonal" @click="this.period = 0">정정</v-btn>
+    </div>
+  </div>
+  <div>
+    <v-text-field label="대출금리" v-model="interest"></v-text-field>
+    <div class="d-flex" style="gap: 0.5rem">
+      <v-btn variant="tonal" @click="incrementInterest(0.1)">0.1%</v-btn>
+      <v-btn variant="tonal" @click="incrementInterest(0.5)">0.5%</v-btn>
+      <v-btn variant="tonal" @click="incrementInterest(1)">1%</v-btn>
+      <v-btn variant="tonal" @click="incrementInterest(5)">5%</v-btn>
+      <v-btn color="error" variant="tonal" @click="this.interest = 0">정정</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      money: 0,
+      period: 0,
+      interest: 0
+    }
+  },
+  methods: {
+    incrementMoney(num) {
+      this.money += num;
+    },
+    incrementPeriod(num) {
+      this.period += num;
+    },
+    incrementInterest(num) {
+      this.interest += num;
+    }
+  },
 }
 </script>
 
@@ -44,14 +59,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
